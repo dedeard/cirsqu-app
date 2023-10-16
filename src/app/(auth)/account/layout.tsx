bec42c2ae3dd11b3bee91b7b8f7d14635da9ae70
Dede Ariansya <me@dedeard.my.id>
2023-10-17 00:00:47 +0800

    🐞 fix: redirect and layout height.

--- src/app/(auth)/account/layout.tsx
+++ src/app/(auth)/account/layout.tsx
@@ -8,14 +8,17 @@ import { usePathname } from 'next/navigation'
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
-  const { initLoading, profile } = useAuth({ whenNotAuthed: '/sign-in?next=' + pathname })
+  const { initLoading, profile } = useAuth({
+    whenNotAuthed: '/sign-in?next=' + pathname,
+    whenAuthedProfileNotExists: '/complete-profile?next=' + pathname,
+  })
   return (
     <>
       <Header />
       {(initLoading || !profile) && <AuthLoading />}
       {!initLoading && profile && (
         <>
-          <div className="flex min-h-screen flex-col">
+          <div className="flex min-h-layout flex-col">
             <div className="container flex flex-col p-3 md:flex-row md:items-start">
               <div className="md:sticky md:top-0">
                 <Sidebar />
