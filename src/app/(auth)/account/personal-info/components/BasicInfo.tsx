2ab69f235f0e7b16bd0530b464c09a1cb38ca080
Dede Ariansya <me@dedeard.my.id>
2023-10-17 14:38:56 +0800

    ✨feature: user can edit email.

---
+++ src/app/(auth)/account/personal-info/components/BasicInfo.tsx
@@ -0,0 +1,142 @@
+'use client'
+import React from 'react'
+import * as yup from 'yup'
+import { Input, Textarea } from '../../components/Input'
+import Button from '../../components/Button'
+import { usernameNotExist } from '@/utils/firestore'
+import { useFormik } from 'formik'
+import { InputAvatar } from '../../components/InputAvatar'
+import clientFetch from '@/utils/client-fetch'
+import toast from 'react-hot-toast'
+import { storageUrl } from '@/utils/firebase'
+import { CardBody, CardFooter, Divider } from '@nextui-org/react'
+import Card from '../../components/Card'
+import { useAuth } from '@/components/contexts/AuthContext'
+
+const getSchema = (username: string) => ({
+  name: yup.string().min(3).max(20).required(),
+  bio: yup.string().max(250),
+  website: yup.string().url(),
+  username: yup
+    .string()
+    .required()
+    .min(6)
+    .max(20)
+    .test('alphanumeric', 'Username must only contain alphanumeric characters.', (value) => /^[a-zA-Z0-9]*$/.test(value))
+    .test('username-unique', 'Username is already taken, please choose another one.', (val) => {
+      if (val.toLocaleLowerCase() === username) return true
+      return usernameNotExist(val)
+    }),
+})
+
+export default function BasicInfo() {
+  const { profile } = useAuth()
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [avatar, setAvatar] = React.useState<File | null>(null)
+  const [avatarError, setAvatarError] = React.useState('')
+
+  const handleFormSubmit = async (data: any) => {
+    setIsLoading(true)
+
+    try {
+      const formData = new FormData()
+      Object.keys(data).forEach((key) => {
+        if (data[key]) formData.append(key, data[key])
+      })
+      if (avatar) {
+        formData.append('avatar', avatar)
+      }
+
+      const response = await clientFetch('profiles', { method: 'PUT', body: formData })
+
+      if (!response.ok) {
+        const data = await response.json()
+        throw new Error(data.message || 'Profile update failed. Please try again.')
+      }
+
+      setAvatar(null)
+      toast.success('Profile updated successfully!')
+    } catch (error: any) {
+      toast.error(`An error occurred: ${error.message}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      name: profile?.name,
+      username: profile?.username,
+      bio: profile?.bio || '',
+      website: profile?.website || '',
+    },
+    onSubmit: handleFormSubmit,
+    validationSchema: yup.object().shape(getSchema(profile?.username || '')),
+  })
+
+  return (
+    // @ts-expect-error
+    <Card as="form" title="Basic Information" className="lg:max-w-3xl" onSubmit={formik.handleSubmit}>
+      <CardBody>
+        <Input
+          label="Name"
+          name="name"
+          placeholder="Enter your name"
+          required
+          value={formik.values.name}
+          errorMessage={formik.errors.name}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        <Divider className="hidden lg:block" />
+        <Input
+          label="Username"
+          name="username"
+          placeholder="Enter your username"
+          required
+          value={formik.values.username}
+          errorMessage={formik.errors.username}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        <Divider className="hidden lg:block" />
+        <InputAvatar
+          label="Avatar"
+          name="avatar"
+          file={avatar}
+          errorMessage={avatarError}
+          nickname={profile?.name || ''}
+          avatar={storageUrl(profile?.avatar)}
+          onFileChange={setAvatar}
+          onValidationError={setAvatarError}
+        />
+        <Divider className="hidden lg:block" />
+        <Input
+          label="Website"
+          name="website"
+          placeholder="Ex: https://dedeard.my.id"
+          value={formik.values.website}
+          errorMessage={formik.errors.website}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        <Divider className="hidden lg:block" />
+        <Textarea
+          label="Bio"
+          name="bio"
+          placeholder="Tell us about yourself..."
+          value={formik.values.bio}
+          errorMessage={formik.errors.bio}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Button type="submit" disabled={isLoading || !!avatarError || Object.keys(formik.errors).length > 0} isLoading={isLoading}>
+          Save
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
---
+++ src/app/(auth)/account/personal-info/components/EmailAddress.tsx
@@ -0,0 +1,94 @@
+'use client'
+import React from 'react'
+import * as yup from 'yup'
+import { Input } from '../../components/Input'
+import Button from '../../components/Button'
+import { useFormik } from 'formik'
+import toast from 'react-hot-toast'
+import { CardBody, CardFooter, Divider } from '@nextui-org/react'
+import Card from '../../components/Card'
+import { useAuth } from '@/components/contexts/AuthContext'
+import { auth } from '@/utils/firebase'
+import { signInWithEmailAndPassword, updateEmail } from 'firebase/auth'
+import FIREBASE_ERRORS from '@/constants/firebase-errors'
+import { Eye, EyeOff } from 'react-feather'
+
+const schema = {
+  email: yup.string().email().required().label('Email address'),
+  password: yup.string().required().label('Password'),
+}
+
+export default function EmailAddress() {
+  const { user } = useAuth()
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isVisible, setIsVisible] = React.useState(false)
+
+  const handleFormSubmit = async (data: { email: string; password: string }) => {
+    setIsLoading(true)
+
+    try {
+      const resp = await signInWithEmailAndPassword(auth, user?.email || '', data.password)
+      await updateEmail(resp.user, data.email)
+
+      toast.success('EMail address updated successfully!')
+    } catch (error: any) {
+      toast.error(FIREBASE_ERRORS[error.code] || `An error occurred: ${error.message}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: user?.email || '',
+      password: '',
+    },
+    onSubmit: handleFormSubmit,
+    validationSchema: yup.object().shape(schema),
+  })
+
+  return (
+    // @ts-expect-error
+    <Card as="form" title="Email Address" className="lg:max-w-3xl" onSubmit={formik.handleSubmit}>
+      <CardBody>
+        <Input
+          label="Email Address"
+          name="email"
+          placeholder="Enter your email"
+          required
+          value={formik.values.email}
+          errorMessage={formik.errors.email}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        <Divider className="hidden lg:block" />
+        <Input
+          label="Current Password"
+          name="password"
+          type={isVisible ? 'text' : 'password'}
+          placeholder="Current password"
+          required
+          value={formik.values.password}
+          errorMessage={formik.errors.password}
+          endContent={
+            <button className="focus:outline-none" type="button" onClick={() => setIsVisible(!isVisible)}>
+              {isVisible ? (
+                <Eye className="pointer-events-none  h-4 text-default-400" />
+              ) : (
+                <EyeOff className="pointer-events-none  h-4 text-default-400" />
+              )}
+            </button>
+          }
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Button type="submit" disabled={isLoading || Object.keys(formik.errors).length > 0} isLoading={isLoading}>
+          Save
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
