62b3e5c2633778370d1000ad7b8f4c56b34e4714
Dede Ariansya <me@dedeard.my.id>
2023-10-17 22:43:55 +0800

    ✨feature: complete not found page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,17 @@
-import Link from 'next/link'
+import Header from '@/components/layout/Header'
+import NotFound from '@/components/layout/NotFound'
+import SimpleFooter from '@/components/layout/SimpleFooter'
 
 export const dynamic = 'force-static'
 
-export default function NotFound() {
+export default function Page() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main className="background-animate flex min-h-screen flex-col dark:bg-gradient-to-br dark:from-black dark:via-gray-950 dark:to-black">
+      <Header onlyBrand shouldHideOnScroll={false} position="static" isBordered={false} />
+
+      <NotFound />
+
+      <SimpleFooter />
+    </main>
   )
 }
