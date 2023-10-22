bf782714f66de1ea51699492a2e437da2185dbfe
Dede Ariansya <me@dedeard.my.id>
2023-10-22 16:33:34 +0800

    🐞 fix: typo.

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
 export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL || 'http://localhost:3000'
 export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || ''
-export const NEXT_PUBLIC_ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_NEXT_PUBLIC_ALGOLIA_APP_ID || ''
+export const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || ''
 export const ALGOLIA_SEARCH_ONLY_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY || ''
