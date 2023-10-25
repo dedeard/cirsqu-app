24b5684ee9cd69d8cde85cbffa414e5393718cf9
Dede Ariansya <me@dedeard.my.id>
2023-10-25 19:37:31 +0800

    ✨feature: create delete confirm component.

---
+++ src/components/elements/DeleteConfirm.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React from 'react'
+import { Modal, ModalContent, ModalBody, Button } from '@nextui-org/react'
+import { XCircle } from 'react-feather'
+
+type PropTypes = {
+  isOpen?: boolean
+  isLoading?: boolean
+  title?: string
+  message?: string
+  onCancel?: () => void
+  onConfirm?: () => void
+}
+
+const DeleteConfirm: React.FC<PropTypes> = ({ isOpen, isLoading, title, message, onCancel, onConfirm }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} placement="center" backdrop="blur" isKeyboardDismissDisabled isDismissable={false} hideCloseButton>
+        <ModalContent>
+          <ModalBody className="p-0">
+            <div className="flex pt-14">
+              <XCircle strokeWidth={0.5} className="m-auto h-28 w-28 text-danger" />
+            </div>
+
+            <div className="p-5 text-center">
+              <h3 className="mb-4 text-3xl font-light">{title || 'Delete Confirmation'}</h3>
+              <p className="mb-6 text-sm">{message || 'Are you sure you want to delete this item? This action cannot be undone.'}</p>
+
+              <div className="flex justify-center gap-4">
+                <Button variant="bordered" className="w-32" isDisabled={isLoading} onPress={onCancel}>
+                  Cancel
+                </Button>
+                <Button className="w-32" color="danger" isLoading={isLoading} onPress={onConfirm}>
+                  {!isLoading && 'Delete'}
+                </Button>
+              </div>
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default DeleteConfirm
--- src/components/features/Comment/CommentList.tsx
+++ src/components/features/Comment/CommentList.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import CommentItem from './CommentItem'
 import cn from 'classnames'
 import { CommentItemSkeleton } from './Skeletons'
-import DeleteCommentConfirm from './DeleteCommentConfirm'
+import DeleteConfirm from '@/components/elements/DeleteConfirm'
+import useCommentActions from './hooks/useCommentActions'
 
 type PropTypes = {
   comments: IComment[]
@@ -12,10 +13,24 @@ type PropTypes = {
 
 const CommentList: React.FC<PropTypes> = ({ comments, className, isLoading, ...props }) => {
   const [deleteQueue, setDeleteQueue] = React.useState<IComment | null>(null)
+  const { isActionInProgress, handleDeleteComment } = useCommentActions(deleteQueue?.commentId || '')
+
+  const handleConfirm = async () => {
+    await handleDeleteComment()
+    setDeleteQueue(null)
+  }
 
   return (
     <>
-      <DeleteCommentConfirm comment={deleteQueue} setDeleteQueue={setDeleteQueue} />
+      <DeleteConfirm
+        isOpen={!!deleteQueue}
+        isLoading={isActionInProgress}
+        title="Are you absolutely certain?"
+        message="Once you delete this comment, it will be gone forever, and cannot be recovered. Please be certain before proceeding."
+        onConfirm={handleConfirm}
+        onCancel={() => setDeleteQueue(null)}
+      />
+
       <ul className={cn(className, 'flex flex-col gap-16 py-10')} {...props}>
         {isLoading
           ? Array.from(Array(3).keys()).map((i) => <CommentItemSkeleton key={i} />)
