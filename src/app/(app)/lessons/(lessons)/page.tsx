2e724890029e2699a2f503cbe2024aeba4603c4b
Dede Ariansya <me@dedeard.my.id>
2023-10-25 16:28:15 +0800

    ✨feature: create lessons page header section.

--- src/app/(app)/lessons/(lessons)/page.tsx
+++ src/app/(app)/lessons/(lessons)/page.tsx
@@ -2,6 +2,7 @@ import { lessonIndex } from '@/utils/algolia'
 import LessonList from '../../components/LessonList'
 import parsePaginationPage from '@/utils/parse-pagination-page'
 import Pagination from '../../components/Pagination'
+import LessonsHeader from './components/LessonsHeader'
 
 export default async function Page({ searchParams }: { searchParams: { page?: string } }) {
   const page = parsePaginationPage(searchParams.page)
@@ -13,6 +14,8 @@ export default async function Page({ searchParams }: { searchParams: { page?: st
 
   return (
     <>
+      <LessonsHeader />
+
       <ul className="mb-3 grid grid-cols-1 gap-3">
         {hits.map((lesson) => (
           <li key={lesson.objectID}>
