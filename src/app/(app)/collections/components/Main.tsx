f726540f26611f4abcaff50e23ad7d78decd2ca9
Dede Ariansya <me@dedeard.my.id>
2023-10-27 01:45:22 +0800

    🐞 fix: collections page auth required.

---
+++ src/app/(app)/collections/components/Main.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React from 'react'
+import { useCollections } from '@/components/contexts/CollectionContext'
+import DeleteConfirm from '@/components/elements/DeleteConfirm'
+import TitleBar from '../../components/TitleBar'
+import LessonList from './LessonList'
+import CollectionEmpty from './Empty'
+import MainLoading from './MainLoading'
+
+const Main: React.FC = () => {
+  const [deleteQueue, setDeleteQueue] = React.useState<string | null>(null)
+  const { collections, loading, actionLoading, removeFromCollection } = useCollections()
+
+  const handleDeleteCollection = async () => {
+    if (!deleteQueue) return
+    await removeFromCollection(deleteQueue)
+    setDeleteQueue(null)
+  }
+
+  if (loading) return <MainLoading />
+
+  return (
+    <>
+      <DeleteConfirm
+        isOpen={!!deleteQueue}
+        isLoading={actionLoading}
+        message="Are you sure you want to delete this item from your collection? Please note that this action cannot be undone."
+        onCancel={() => setDeleteQueue(null)}
+        onConfirm={handleDeleteCollection}
+      />
+
+      <TitleBar title={`Collections ${collections.length || ''}`} className="mb-3" />
+
+      <CollectionEmpty />
+
+      <ul className="grid grid-cols-1 gap-3" role="list">
+        {collections.map(({ collectionId, lesson }) => (
+          <React.Fragment key={collectionId}>
+            {lesson && <LessonList lesson={lesson} collectionId={collectionId} setDeleteQueue={setDeleteQueue} />}
+          </React.Fragment>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Main
--- src/app/(app)/collections/page.tsx
+++ src/app/(app)/collections/page.tsx
@@ -1,44 +1,14 @@
 'use client'
 import React from 'react'
-import { useCollections } from '@/components/contexts/CollectionContext'
-import DeleteConfirm from '@/components/elements/DeleteConfirm'
-import TitleBar from '../components/TitleBar'
-import LessonList, { LessonListSkeleton } from './components/LessonList'
-import CollectionEmpty from './components/Empty'
+import { useAuth } from '@/components/contexts/AuthContext'
+import MainLoading from './components/MainLoading'
+import Main from './components/Main'
 
 export default function Page() {
-  const [deleteQueue, setDeleteQueue] = React.useState<string | null>(null)
-  const { collections, loading, actionLoading, removeFromCollection } = useCollections()
+  const { initLoading } = useAuth({
+    whenNotAuthed: '/sign-in?next=/collections',
+    whenAuthedProfileNotExists: '/complete-profile?next=/collections',
+  })
 
-  const handleDeleteCollection = async () => {
-    if (!deleteQueue) return
-    await removeFromCollection(deleteQueue)
-    setDeleteQueue(null)
-  }
-
-  return (
-    <>
-      <DeleteConfirm
-        isOpen={!!deleteQueue}
-        isLoading={actionLoading}
-        message="Are you sure you want to delete this item from your collection? Please note that this action cannot be undone."
-        onCancel={() => setDeleteQueue(null)}
-        onConfirm={handleDeleteCollection}
-      />
-
-      <TitleBar title={`Collections ${collections.length || ''}`} isLoading={loading} className="mb-3" />
-
-      <CollectionEmpty />
-
-      <ul className="grid grid-cols-1 gap-3" role="list">
-        {collections.map(({ collectionId, lesson }) => (
-          <React.Fragment key={collectionId}>
-            {lesson && <LessonList lesson={lesson} collectionId={collectionId} setDeleteQueue={setDeleteQueue} />}
-          </React.Fragment>
-        ))}
-
-        {loading && Array.from(Array(4).keys()).map((i) => <LessonListSkeleton key={i} />)}
-      </ul>
-    </>
-  )
+  return initLoading ? <MainLoading /> : <Main />
 }
