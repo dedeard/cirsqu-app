5fcbf80d8571d8e0fb208a4239e3ffc0a6f01d32
Dede Ariansya <me@dedeard.my.id>
2023-10-31 19:18:39 +0800

    🐞 fix: enable  `@vercel/analytics`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import 'highlight.js/styles/github-dark.min.css'
+import { Analytics } from '@vercel/analytics/react'
 import Providers from './Providers'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
@@ -7,6 +8,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en" className="dark" style={{ colorScheme: 'dark' }}>
       <body className="overflow-x-hidden overflow-y-scroll antialiased">
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   )
