6c7d88be11b33ab491d8d3d34fd296fe49afc8fc
Dede Ariansya <me@dedeard.my.id>
2023-11-09 21:13:43 +0800

    🖥️ wip: update pagination style.

--- src/app/(app)/components/Pagination.tsx
+++ src/app/(app)/components/Pagination.tsx
@@ -1,38 +1,34 @@
 'use client'
-import { Button } from '@nextui-org/react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React from 'react'
-import { ArrowLeft, ArrowRight } from 'react-feather'
+import Link from 'next/link'
+import { ChevronLeft, ChevronRight } from 'react-feather'
+import { usePathname } from 'next/navigation'
 
 const Pagination: React.FC<{ totalPage: number; page: number }> = ({ totalPage, page }) => {
   const pathname = usePathname()
   if (totalPage <= 1) return null
 
   return (
-    <div className="flex justify-between">
-      <Button
-        size="lg"
-        variant="flat"
-        className="uppercase"
-        as={page <= 1 ? 'span' : Link}
-        href={`${pathname}${page > 2 ? '?page=' + (page - 1) : ''}`}
-        isDisabled={page <= 1}
-        startContent={<ArrowLeft />}
-      >
-        Prev <span className="hidden md:inline">Page</span>
-      </Button>
-      <Button
-        size="lg"
-        variant="flat"
-        className="uppercase"
-        as={page >= totalPage ? 'span' : Link}
-        href={`${pathname}?page=${page + 1}`}
-        isDisabled={page >= totalPage}
-        endContent={<ArrowRight />}
-      >
-        Next <span className="hidden md:inline">Page</span>
-      </Button>
+    <div className="flex">
+      {!(page <= 1) && (
+        <Link
+          className="hoverable-default mr-auto flex items-center rounded-lg border p-3 pr-5"
+          href={`${pathname}${page > 2 ? '?page=' + (page - 1) : ''}`}
+        >
+          <ChevronLeft size={20} className="mr-3" />
+          <span>
+            Prev <span className="hidden md:inline">Page</span>
+          </span>
+        </Link>
+      )}
+      {!(page >= totalPage) && (
+        <Link className="hoverable-default ml-auto flex items-center rounded-lg border p-3 pl-5" href={`${pathname}?page=${page + 1}`}>
+          <span>
+            Next <span className="hidden md:inline">Page</span>
+          </span>
+          <ChevronRight size={20} className="ml-3" />
+        </Link>
+      )}
     </div>
   )
 }
