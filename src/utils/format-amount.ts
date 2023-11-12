c8b16c23e5fe6f8428ff64d33809f453f998f7ac
Dede Ariansya <me@dedeard.my.id>
2023-11-12 11:08:36 +0800

    ⏪ rollback: remove currency-symbol-map.

--- src/utils/format-amount.ts
+++ src/utils/format-amount.ts
@@ -1,7 +1,26 @@
-import getSymbolFromCurrency from 'currency-symbol-map'
+const currencySymbols: Record<string, string> = {
+  USD: '$',
+  EUR: '€',
+  GBP: '£',
+  JPY: '¥',
+  CNY: '¥',
+  KRW: '₩',
+  INR: '₹',
+  RUB: '₽',
+  BRL: 'R$',
+  AUD: 'A$',
+  CAD: 'C$',
+  IDR: 'Rp',
+}
+
+const getSymbolFromCurrency = (code: string) => {
+  const symbol = currencySymbols[code.toUpperCase()]
+  return symbol || code
+}
 
 export default function formatAmount(amount: number, currencyCode: string = 'usd', trimZero: boolean = true) {
   const currencySymbol = getSymbolFromCurrency(currencyCode)
+
   let result = (amount / 100).toFixed(2)
 
   // Remove .00 at the end
