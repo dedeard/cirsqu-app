1aa9c7a4cbed1f69ec908a6280251a3d452dde95
Dede Ariansya <me@dedeard.my.id>
2023-11-15 12:54:59 +0800

    🐞 fix: remove unused vars.

--- src/app/images/[type]/[size]/icon.png/route.tsx
+++ src/app/images/[type]/[size]/icon.png/route.tsx
@@ -4,19 +4,15 @@ import Logo from '@/components/svg/Logo'
 export const runtime = 'edge'
 
 export async function GET(request: Request, { params }: { params: { type: string; size: string } }) {
-  let size = Number(params.size)
-  let inner = 80
-
-  if (isNaN(size)) size = 192
-  else if (size <= 30) size = 30
-  else if (size >= 1024) size = 1024
-
-  if (params.type === 'maskable') inner = 50
-
   try {
-    const { searchParams } = new URL(request.url)
-    const hasTitle = searchParams.has('title')
-    const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'My default title'
+    let size = Number(params.size)
+    let inner = 80
+
+    if (isNaN(size)) size = 192
+    else if (size <= 30) size = 30
+    else if (size >= 1024) size = 1024
+
+    if (params.type === 'maskable') inner = 50
 
     return new ImageResponse(
       (
