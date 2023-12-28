8c90ddffa348db21b549a00a5db3decaceec1c66
Dede Ariansya <me@dedeard.my.id>
2023-12-28 22:42:58 +0800

    🖥️ wip: complete question item.

--- src/app/(app)/forum/(forum)/(globals)/page.tsx
+++ src/app/(app)/forum/(forum)/(globals)/page.tsx
@@ -1,3 +1,13 @@
+import QuestionItem from '../components/QuestionItem'
+
 export default function ForumPage() {
-  return <></>
+  return (
+    <ul className="flex flex-col gap-3 pt-3 xl:pb-3">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <li key={i}>
+          <QuestionItem question={{}} />
+        </li>
+      ))}
+    </ul>
+  )
 }
--- src/app/(app)/forum/(forum)/layout.tsx
+++ src/app/(app)/forum/(forum)/layout.tsx
@@ -107,7 +107,7 @@ const rawTags = [
 export default function ForumLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col gap-3 xl:flex-row">
-      <div className="pt-3 xl:flex-1 xl:py-3">
+      <div className="pt-3 xl:flex-1">
         <Tab />
         <>{children}</>
       </div>
