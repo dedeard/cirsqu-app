3719b7471ea71ed6d6a8cf0705e7f07530a552e0
Dede ard <me@dedeard.my.id>
2025-04-18 10:13:29 +0800

    fix: adjust padding in LayoutCard component

--- src/app/(auth)/components/LayoutCard.tsx
+++ src/app/(auth)/components/LayoutCard.tsx
@@ -10,7 +10,7 @@ const LayoutCard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <div className="relative overflow-hidden rounded-lg border border-neutral-200 bg-neutral-200/30 dark:border-neutral-800 dark:bg-neutral-800/30">
       <LoadingScreen show={loading || initLoading || !!profile} />
       <AuthTab />
-      <div className="pb-5 pt-10 md:px-10 md:pb-10 first:md:pt-14">{children}</div>
+      <div className="px-3 pb-5 pt-10 md:px-10 md:pb-10 first:md:pt-14">{children}</div>
     </div>
   )
 }
